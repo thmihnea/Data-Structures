2a8e91b6b41c3c11704a6e8427cb3a442953291e
thmihnea <mihneathm@gmail.com>
2024-09-21 15:39:39 +0300

    initial bst impl

---
+++ src/tree/BinarySearchTree.h
@@ -0,0 +1,59 @@
+#ifndef DS_BINARY_SEARCH_TREE_H
+#define DS_BINARY_SEARCH_TREE_H
+
+#include <type_traits>
+#include <optional>
+
+template <typename T, typename = void>
+struct is_comparable : std::false_type {};
+
+template <typename T>
+struct is_comparable<T, std::void_t<decltype(std::declval<T>() < std::declval<T>())>> : std::true_type {};
+
+template <typename T>
+constexpr bool is_comparable_v = is_comparable<T>::value;
+
+template <typename K, typename V, typename = std::enable_if_t<is_comparable_v<K>>>
+class Node
+{
+private:
+    Node<K, V>* left;
+    Node<K, V>* right;
+    K key;
+    V value;
+
+public:
+    Node(const K& key, const V& value);
+
+    ~Node() = default;
+
+    K key();
+
+    V value();
+
+    Node<K, V>* left();
+
+    Node<K, V>* right();
+
+    void set_left(Node<K, V>* node);
+    
+    void set_right(Node<K, V>* right);
+};
+
+template <typename K, typename V, typename = std::enable_if_t<is_comparable_v<K>>>
+class BinarySearchTree
+{
+private:
+    Node<K, V>* root;
+
+public:
+    BinarySearchTree();
+
+    ~BinarySearchTree();
+
+    void put(const K& key, const V& value);
+
+    std::optional<V> get(const K& key);
+};
+
+#endif
